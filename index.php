048244e5c18f6c7d9da08324395ad016066a6721
cleciobr <cleciope@gmail.com>
2022-07-18 20:58:02 -0300

    update

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-phpinfo();
+require __DIR__ . '/vendor/autoload.php';
+include __DIR__ . '/Db/Sql.php';
 include __DIR__ . '/includes/header.php';
 include __DIR__ . '/includes/body.php';
 include __DIR__ . '/includes/footer.php';
