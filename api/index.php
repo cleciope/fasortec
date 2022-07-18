487c53b91d69aee99234a914b14091d25de6e36f
cleciobr <cleciope@gmail.com>
2022-07-18 20:54:37 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,5 @@
 <?php
 phpinfo();
-require __DIR__ . '/vendor/autoload.php';
-include __DIR__ . '/Db/Sql.php';
 include __DIR__ . '/includes/header.php';
 include __DIR__ . '/includes/body.php';
 include __DIR__ . '/includes/footer.php';
