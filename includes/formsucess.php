36eebee95807938f5c866294456b798eb24d1766
Clécio Lins <cleciope@gmail.com>
2022-07-25 17:49:23 -0300

    Update formsucess.php

--- includes/formsucess.php
+++ includes/formsucess.php
@@ -1,22 +1,33 @@
-         <div class="text-icons">
-           <h3 class="h3 mb-3">Temos equipe de várias áreas para suprir a necessidade da sua empresa</h3>
-           <p>
-             <hr>
-<?php
-/* Recebemos os parâmetro no começo do script  */
-$name = ( isset($_POST['name']) ) ? $_POST['name'] : null;
-
-/* A partir deste ponto podemos utilizar as variáveis  */
-echo $name;
-
-?>
-             <span style="font-weight: bold;">"Coisas incríveis no mundo dos negócios
-               nunca são feitas por uma única pessoa, e sim por uma equipe."</span>
-
-           <div align="right" style="font-weight: bold;">- Steve Jobs.</div>
-           </p>
-         </div>
-         <div class="form-group col-md-6" style="display: flex;
-            justify-content: center; width:500px; height: 450px; margin-top: 4rem;">
-           <img src=" /res/site/img/atendente.png"></img>
-         </div>
+ <section id="form">
+          
+            <br>
+            <br>
+            <div class="form-row">
+             <div class="form-group col-md-6">
+              <br>
+              <div style="text-align: center">
+            
+              <?php
+                  /* Recebemos os parâmetro no começo do script  */
+                  $name = ( isset($_POST['name']) ) ? $_POST['name'] : null;
+                  $email = ( isset($_POST['mail']) ) ? $_POST['mail'] : null;
+                  /* A partir deste ponto podemos utilizar as variáveis  */
+                  $texto = "Obrigado,";
+              ?>
+              <h3>
+               <?php echo $texto ?></h3><h4 style="font-weight: bold;"><?php echo $name.'.'; ?></h4>
+               <h4>Sua mensagem foi enviada com êxito!</h4>
+                  <h4 style="text-align: center">Responderemos o mais breve possível para o e-mail informado abaixo:</h4><h5 h4 style="font-weight: bold;"><?php echo $email;?></h5>
+              </div>
+            </div>
+            <div class="form-group col-md-6" 
+            style="
+            display: flex;
+             
+            width:550px; 
+            height: 450px; 
+            margin-top: 1px;
+            ">
+              <img src=" /public/img/atendente.png"></img>
+            </div>
+   </section>
